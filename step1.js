e674e1a7b8e68c67244dea9fe17647829ab37385
Lexsa Campbell <lexsacampbell@gmail.com>
2022-11-07 15:13:24 -0800

    Completed steps 1 and 2 of the node.js problem set

---
+++ step1.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const process = require('process');
+
+function cat(path) {
+    fs.readFile(path, 'utf8', function(err, data) {
+        if (err) {
+            // handle possible error
+            console.error(err);
+            // kill the process and tell the shell it errored
+            process.exit(1);
+          }
+          // otherwise success
+          console.log(`file contents: ${data}`);
+        });
+        
+        console.log('reading file');
+        // file won't have been read yet at this point        
+};
+
+cat(process.argv[2]);
